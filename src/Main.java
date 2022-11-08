9e00ffe28db917a2b16843f7f5e755377de01703
Tatyana58 <tatbir@rambler.ru>
2022-11-08 20:13:05 +0300

    Clean file Addition and subtraction

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Actions on variables (Изучение переменных и их действия)");
+
+        var dog=8.0;
+        System.out.println("dog ="+dog);
+        var cat = 3.6;
+        System.out.println("cat = "+cat);
+        var paper = 763789;
+        System.out.println("paper = "+paper);
+        dog=dog+4;
+        System.out.println("increased the value of the variable by + 4");
+        System.out.println("dog ="+dog);
+        cat = cat+4;
+        System.out.println("cat = "+cat);
+        paper = paper+4;
+        System.out.println("paper = "+paper);
+        System.out.println("reduced the value of the variable by : ");
+        dog=dog-3.5;
+        System.out.println("dog -3,5  ="+dog);
+        cat=cat-1.6;
+        System.out.println("cat - 1,6 = "+cat);
+        paper=paper-7639;
+        System.out.println("paper - 7639 = "+paper);
     }
 }
